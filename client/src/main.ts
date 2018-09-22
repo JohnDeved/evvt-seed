0a50da00c1ac836d117248e1e9bd359b86c36192
Undefined <johann@objekt.stream>
2018-09-22 19:57:44 +0200

    + vuesax & dev scripts

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import Vuesax from 'vuesax'
+
+import 'material-icons/iconfont/material-icons.css'
+import 'vuesax/dist/vuesax.css'
+Vue.use(Vuesax)
 
 Vue.config.productionTip = false
 
--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <vs-button vs-type="filled">Hello World</vs-button>
   </div>
 </template>
